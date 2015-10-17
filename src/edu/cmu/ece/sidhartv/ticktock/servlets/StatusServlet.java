7ee18ad96f8ee61ad6c7473f5c11da1eb6e7a8cc
Sidharth Verma <sidhartv@andrew.cmu.edu>
2015-10-17 05:07:37 -0400

    Wrote doGet() for StatusServlet

--- src/edu/cmu/ece/sidhartv/ticktock/servlets/StatusServlet.java
+++ src/edu/cmu/ece/sidhartv/ticktock/servlets/StatusServlet.java
@@ -1,10 +1,11 @@
 package edu.cmu.ece.sidhartv.ticktock.servlets;
 
 import java.io.IOException;
-
-
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
@@ -12,6 +13,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Servlet implementation class StatusServlet
@@ -34,19 +40,83 @@ public class StatusServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection connection = null;
+		PrintWriter responseOut = response.getWriter();
+		
+		PrintStream errorOut = System.err;
+		
 		try {
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
 			} catch (ClassNotFoundException e) {
 				response.getWriter().append("Error: driver cannot be found\n");
 			}
-			Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/TickTock", "root", "");
-			response.getWriter().append("Connection successful");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/TickTock", "root", "");
+			errorOut.append("Connection successful");
 		} catch (SQLException e) {
-			response.getWriter().append("Failed to connect to MySQL database: \n\t"
+			errorOut.append("Failed to connect to MySQL database: \n\t"
 					+ e.getMessage()
 					+ "\n");
+			response.setStatus(500);
 		}
+		Statement statement = null;
+		int requestedUserId = -1;
+		try {
+			requestedUserId = Integer.parseInt(request.getParameter("uid"));
+		} catch (NumberFormatException e) {
+			errorOut.append("User ID was not a valid number");
+			response.setStatus(500);
+		}
+		
+		String dateTimeRequested = (request.getParameter("time"));
+		SimpleDateFormat mySQLFormatDateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		Date dateToUse = null;
+		if (dateTimeRequested.equals("current")) {
+			dateToUse = new Date(System.currentTimeMillis());
+		} else {
+			try {
+				dateToUse = mySQLFormatDateTime.parse(dateTimeRequested);
+			} catch (ParseException e) {
+				errorOut.append("Could not parse the provided time\n");
+				response.setStatus(500);
+			}
+		}
+		try {
+			statement = connection.createStatement();
+		} catch (SQLException e) {
+			errorOut.append("Statement creation failed\n");
+			response.setStatus(500);
+		}
+		String dateToUseString = mySQLFormatDateTime.format(dateToUse);
+		String query = "SELECT 'status' FROM 'statuses' WHERE 'userID'= " + requestedUserId + " AND 'timeposted' > " + dateToUseString + " ORDER BY 'timeposted' DESC";
+		ResultSet queryResultSet = null;
+		try {
+			queryResultSet = statement.executeQuery(query);
+		} catch (SQLException e) {
+			errorOut.append("Query failed\n");
+			response.setStatus(500);
+		}
+		String statusToRespond = "";
+		String timePosted = "";
+		try {
+			if (queryResultSet.first()) {
+				statusToRespond.concat(queryResultSet.getNString("status"));
+				timePosted.concat(queryResultSet.getNString("timePosted"));
+			} else {
+				statusToRespond = null;
+				timePosted = null;
+			}
+		} catch (SQLException e) {
+			errorOut.append("Something went wrong...somehow...whoops");
+			response.setStatus(500);
+		}
+		response.setContentType("text/json");
+		responseOut.append("{ \n"
+								+ "\t\"status\": " + statusToRespond + "\n"
+								+ "\t\"timePosted\": " + timePosted  + "\n"
+						+ "}");
+		response.setStatus(200);
 		
 		
 	}
